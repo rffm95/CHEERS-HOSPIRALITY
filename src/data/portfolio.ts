1f619ed096c557aa0cc3481bb27142b316004d0c
rffm95 <sucessomacico@gmail.com>
2026-08-20 16:28:55 +0100

    Add multiple portfolio items with images

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -16,9 +16,39 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'EXEMPLO TRABALHO DIGITAL',
     image: 'https://i.imgur.com/CnaTvqH.png',
   },
-  {
+   {
     id: '3',
     title: 'EXEMPLO TRABALHO DIGITAL',
+    image: 'https://i.imgur.com/JzfPypn.png',
+  },
+  {
+    id: '4',
+    title: 'EXEMPLO TRABALHO DIGITAL',
     image: 'https://i.imgur.com/VrbFY65.png',
+  },
+    {
+    id: '5',
+    title: 'EXEMPLO TRABALHO DIGITAL',
+    image: 'https://i.imgur.com/VEnpjnY.png',
+  },
+    {
+    id: '6',
+    title: 'EXEMPLO TRABALHO DIGITAL',
+    image: 'https://i.imgur.com/n993YwC.png',
+  },
+    {
+    id: '7',
+    title: 'EXEMPLO TRABALHO DIGITAL',
+    image: 'https://i.imgur.com/gLkKjHL.png',
+  },
+    {
+    id: '8',
+    title: 'EXEMPLO TRABALHO DIGITAL',
+    image: 'https://i.imgur.com/BFUQH2t.png',
+  },
+    {
+    id: '9',
+    title: 'EXEMPLO TRABALHO DIGITAL',
+    image: 'https://i.imgur.com/sC49CxI.png',
   },
 ];
